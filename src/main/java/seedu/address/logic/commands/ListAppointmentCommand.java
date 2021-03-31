1da3269df74ed4bd89ceabafa5c9c49bb8df9b85
Stratostorm <h_weicong@outlook.com>
2021-03-31 14:00:32 +0800

    Clean up ListAppointmentCommand class

--- src/main/java/seedu/address/logic/commands/ListAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/ListAppointmentCommand.java
@@ -1,18 +1,19 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_OPTION;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPOINTMENTS;
 
 import seedu.address.model.Model;
 
+/**
+ * Lists all appointments in the appointment book to the user.
+ */
 public class ListAppointmentCommand extends Command {
     public static final String COMMAND_WORD = "listAppt";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Lists appointments in the appointment book.\n"
-            + "Parameters: [" + PREFIX_OPTION + "OPTION]\n";
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+            + ": Lists appointments in the appointment book.\n";
+    public static final String MESSAGE_SUCCESS = "Listed all appointments";
 
     public ListAppointmentCommand() {}
 
